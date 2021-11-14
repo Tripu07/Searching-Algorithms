87414c84849e19327ba60db38fc78c906a2df8a9
Tripu07 <87227815+Tripu07@users.noreply.github.com>
2021-11-14 17:37:31 +0530

    Update BinarySearch.py

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,19 +1,23 @@
 def binary_search(a,x,start,end):
-    b=(start+end)//2
-    if(a[start]==x):
-        return start
-    elif(a[end]==x):
-        return end
-    elif(a[b]==x):
-         return b
-    elif(a[start]!=x and a[end]!=x):
-        if(a[b]>x):
-            return binary_search(a,x,start,b)
-        else:
-            return binary_search(a,x,b,end)
+    if x in a:
+        
+        b=(start+end)//2
+        if(a[start]==x):
+            return start
+        elif(a[end]==x):
+            return end
+        elif(a[b]==x):
+            return b
+        elif(a[start]!=x and a[end]!=x):
+            if(a[b]>x):
+                return binary_search(a,x,start,b)
+            else:
+                return binary_search(a,x,b,end)
     
     else:
-        return "Not"
+        return "Not Present in the list"
 a=[1,2,3,4,5,5,9,4,24,435,2342,2524,500000]
 n=len(a)
+
 print(binary_search(a,24,0,n-1))
+print(binary_search(a,2676,0,n-1))
